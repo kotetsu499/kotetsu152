da344fa9c555c866bf8d93395f2eb1cb9210781f
kotetsu499 <156553621+kotetsu499@users.noreply.github.com>
2024-05-09 17:50:26 +0900

    素数判定python版
    pthon!!!!!!!!!!!!!!!!!!!!!!!

--- prime.py
+++ prime.py
@@ -1,18 +1,19 @@
-
+import math
 while True:
-    prime = int(input("判定したい素数を入力..."))
-    warukazu = 2
-    while True:
-        if prime==warukazu:
-            print("素数です")
-            break
-        else:
-            if prime % warukazu == 0:
-                print("素数じゃない")
+    prime = input("判定したい素数を入力(数字以外で終了)...")
+    if prime.isdigit():
+        prime = int(prime)
+        warukazu = 2
+        max = math.sqrt(prime)
+        while True:
+            if warukazu>max:
+                print("素数です")
                 break
             else:
-                warukazu+=1
-
-            
-
-    
+                if prime % warukazu == 0:
+                    print("素数ではありません。少なくとも",warukazu,"で割り切れます")
+                    break
+                else:
+                    warukazu+=1
+    else:
+        exit()
